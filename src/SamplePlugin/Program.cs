342a393ca305d16d017bae49bf66345c05711043
Carey Payette <carey.payette@gmail.com>
2019-01-18 21:37:58 -0500

    Tested and fixed starting the Stream Deck plugin via command line arguments.

--- src/SamplePlugin/Program.cs
+++ src/SamplePlugin/Program.cs
@@ -13,55 +13,69 @@ using System.Diagnostics;
 
 namespace SamplePlugin
 {
-	class Program
+  class Program
+  {
+	static ILoggerFactory GetLoggerFactory()
 	{
-        static ILoggerFactory GetLoggerFactory()
-        {
-            Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-            
-            var configuration = new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json")
-                    .Build();
+	  Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
 
-            Log.Logger = new LoggerConfiguration()
-                    .ReadFrom.Configuration(configuration)
-                    .CreateLogger();
+	  var configuration = new ConfigurationBuilder()
+			  .AddJsonFile("appsettings.json")
+			  .Build();
 
-            var loggerFactory = new LoggerFactory()
-                .AddSerilog(Log.Logger);
+	  Log.Logger = new LoggerConfiguration()
+			  .ReadFrom.Configuration(configuration)
+			  .CreateLogger();
 
-            TopLogger = loggerFactory.CreateLogger("top");
+	  var loggerFactory = new LoggerFactory()
+		  .AddSerilog(Log.Logger);
 
-            TopLogger.LogInformation("Plugin started");
-            
-            return loggerFactory;
-        }
+	  TopLogger = loggerFactory.CreateLogger("top");
 
-        private static Microsoft.Extensions.Logging.ILogger TopLogger;
+	  TopLogger.LogInformation("Plugin started");
 
-        static async Task Main(string[] args)
-        {
-            using (var source = new CancellationTokenSource())
-            {
-                using (var loggerFactory = GetLoggerFactory())
-                {
-                    try
-                    {
-                        using (var conMan = ConnectionManager.Initialize(args, loggerFactory))
-                        {
-                            await conMan.SetPlugin(new MySamplePlugin())
-                                .StartAsync(source.Token);
-                        }
-
-                        Console.ReadLine();
-                    }
-                    catch (Exception ex)
-                    {
-                        TopLogger.LogError(ex, "Error while running the plugin");
-                    }
-                }
-                source.Cancel();
-            }
-		}
+	  return loggerFactory;
 	}
+
+	private static Microsoft.Extensions.Logging.ILogger TopLogger;
+
+	static async Task Main(string[] args)
+	{
+
+
+#if DEBUG
+
+	  Debugger.Launch();
+
+	  while (!Debugger.IsAttached)
+	  {
+		await Task.Delay(100);
+	  }
+
+#endif
+	  using (var source = new CancellationTokenSource())
+	  {
+		using (var loggerFactory = GetLoggerFactory())
+		{
+		  try
+		  {
+
+			// codingbandit: I had to take out the using statement as it was causing the dispose method to be called (and disposing the socket connection)
+			await ConnectionManager.Initialize(args, loggerFactory)
+				.SetPlugin(new MySamplePlugin())
+				.StartAsync(source.Token);
+
+			Console.ReadLine();
+
+		  }
+		  catch (Exception ex)
+		  {
+			TopLogger.LogError(ex, "Error while running the plugin");
+		  }
+		  source.Cancel();
+		}
+
+	  }
+	}
+  }
 }
