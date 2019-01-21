5deb03c385268899877adde32dc9f907847c5f0e
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-01-21 11:50:35 -0500

    Implemented the rest of the events the StreamDeck App can send our plugin

---
+++ src/StreamDeckLib/Messages/Coordinates.cs
@@ -0,0 +1,15 @@
+﻿namespace StreamDeckLib.Messages
+{
+	public class Coordinates
+	{
+		public int column { get; set; }
+		public int row { get; set; }
+	}
+
+	public class Size
+	{
+		public int columns { get; set; }
+		public int rows { get; set; }
+	}
+
+}
