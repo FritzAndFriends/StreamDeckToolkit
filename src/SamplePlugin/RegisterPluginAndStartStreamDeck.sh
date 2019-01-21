c40efc9666aad13d53c28fa590c968732597055d
Brady Gaster <bradyg@microsoft.com>
2019-01-21 15:41:01 -0800

    finalized mac inner loop

--- src/SamplePlugin/RegisterPluginAndStartStreamDeck.sh
+++ src/SamplePlugin/RegisterPluginAndStartStreamDeck.sh
@@ -3,5 +3,8 @@ pkill 'Stream Deck'
 uuid=$(jq .Actions[0].UUID manifest.json)
 suffix=".action"
 pluginName=$(echo "$uuid" | tr -d \" | sed -e "s/$suffix$//")
-echo $pluginName
+echo 'Installing the $pluginName plugin...'
+rm -r ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
+mkdir ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
+cp bin/Debug/netcoreapp2.2/* ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
 exit
