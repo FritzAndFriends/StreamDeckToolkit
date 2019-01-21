582f22bb02ae8bdd51b117079183759a0ef4893b
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-01-21 11:59:23 -0500

    Revert "Implement StreamDeck Events Received"

--- src/StreamDeckLib/BaseStreamDeckPlugin.cs
+++ src/StreamDeckLib/BaseStreamDeckPlugin.cs
@@ -1,29 +1,15 @@
 ﻿using StreamDeckLib.Messages;
-using System;
 using System.Threading.Tasks;
 
 namespace StreamDeckLib
 {
-	public class BaseStreamDeckPlugin
+    public class BaseStreamDeckPlugin
 	{
 		protected internal ConnectionManager Manager { get; set; }
-
-		public virtual Task OnKeyDown(StreamDeckEventPayload args) => Task.CompletedTask;
-
-		public virtual Task OnKeyUp(StreamDeckEventPayload args) => Task.CompletedTask;
-
-		public virtual Task OnWillAppear(StreamDeckEventPayload args) => Task.CompletedTask;
-
-		public virtual Task OnWillDisappear(StreamDeckEventPayload args) => Task.CompletedTask;
-
-		public virtual Task OnTitleParametersDidChange(StreamDeckEventPayload args) => Task.CompletedTask;
-
-		public virtual Task OnDeviceDidConnect(StreamDeckEventPayload args) => Task.CompletedTask;
-
-		public virtual Task OnDeviceDidDisconnect(StreamDeckEventPayload args) => Task.CompletedTask;
-
-		public virtual Task OnApplicationDidLaunch(StreamDeckEventPayload args) => Task.CompletedTask;
-
-		public virtual Task OnApplicationDidTerminate(StreamDeckEventPayload args) => Task.CompletedTask;
-	}
+        public virtual Task OnKeyDown(StreamDeckEventPayload args) => Task.CompletedTask;
+        public virtual Task OnKeyUp(StreamDeckEventPayload args) => Task.CompletedTask;
+        public virtual Task OnWillAppear(StreamDeckEventPayload args) => Task.CompletedTask;
+        public virtual Task OnWillDisappear(StreamDeckEventPayload args) => Task.CompletedTask;
+        public virtual Task OnTitleParametersDidChange(StreamDeckEventPayload args) => Task.CompletedTask;
+    }
 }
