086c7a64d729f0616cff0ec0bd669e6d94e43826
Brady Gaster <41929050+bradygaster@users.noreply.github.com>
2019-01-21 18:59:20 -0800

    now it will compile (#44)
    * started working on fixing the dotnet new experience for mac
    
    * fixed compilation
    
    * added debug capability, not quite there yet

---
+++ Templates/StreamDeck.PluginTemplate.Csharp/content/RegisterPluginAndStartStreamDeck.ps1
@@ -0,0 +1,22 @@
+﻿
+$streamDeckExePath = "C:\Program Files\Elgato\StreamDeck\StreamDeck.exe"
+$bindir = $PSScriptRoot + "\bin\Debug\netcoreapp2.2"
+$manifestFile = $bindir +"\manifest.json"
+
+$manifestContent = Get-Content $manifestFile | Out-String
+$json = ConvertFrom-JSON $manifestcontent
+$uuidAction = $json.Actions[0].UUID
+$pluginID = $uuidAction.substring(0, $uuidAction.Length - ".action".Length)
+$destDir = $env:APPDATA + "\Elgato\StreamDeck\Plugins\" + $pluginID + ".sdPlugin"
+
+$pluginName = split-path $PSScriptRoot -leaf
+
+Get-Process StreamDeck,$pluginName | Stop-Process –force -ErrorAction SilentlyContinue 
+
+mkdir $destDir -ErrorAction SilentlyContinue
+Get-ChildItem -Path $bindir -Recurse | ? { $_.FullName -inotmatch '.vs' } | Copy-Item -Destination $destDir
+
+Write-Host "Script Completed"
+
+#& $streamDeckExePath
+exit 0
--- src/StreamDeckLib/ConnectionManager.cs
+++ src/StreamDeckLib/ConnectionManager.cs
@@ -12,253 +12,253 @@ using System.Threading.Tasks;
 
 namespace StreamDeckLib
 {
-  /// <summary>
-  /// This class manages the connection to the StreamDeck hardware
-  /// </summary>
-  public partial class ConnectionManager : IDisposable
-  {
-	private int _Port;
-	private string _Uuid;
-	private string _RegisterEvent;
-	private readonly ClientWebSocket _Socket = new ClientWebSocket();
-	private ConnectionManager() { }
-	public Messages.Info Info { get; private set; }
-	public static ConnectionManager Initialize(string[] commandLineArgs,
-		ILoggerFactory loggerFactory = null)
-	{
-	  using (var app = new CommandLineApplication())
-	  {
-		app.HelpOption();
-		
-		var optionPort = app.Option<int>("-port|--port <PORT>", "The port the Elgato StreamDeck software is listening on", CommandOptionType.SingleValue);
-		var optionPluginUUID = app.Option("-pluginUUID <UUID>", "The UUID that the Elgato StreamDeck software knows this plugin as.", CommandOptionType.SingleValue);
-		var optionRegisterEvent = app.Option("-registerEvent <REGEVENT>", "The registration event", CommandOptionType.SingleValue);
-		var optionInfo = app.Option("-info <INFO>", "Some information", CommandOptionType.SingleValue);
+    /// <summary>
+    /// This class manages the connection to the StreamDeck hardware
+    /// </summary>
+    public partial class ConnectionManager : IDisposable
+    {
+        private int _Port;
+        private string _Uuid;
+        private string _RegisterEvent;
+        private readonly ClientWebSocket _Socket = new ClientWebSocket();
+        private ConnectionManager() { }
+        public Messages.Info Info { get; private set; }
+        public static ConnectionManager Initialize(string[] commandLineArgs,
+            ILoggerFactory loggerFactory = null)
+        {
+            using (var app = new CommandLineApplication())
+            {
+                app.HelpOption();
 
-		app.Parse(commandLineArgs);
+                var optionPort = app.Option<int>("-port|--port <PORT>", "The port the Elgato StreamDeck software is listening on", CommandOptionType.SingleValue);
+                var optionPluginUUID = app.Option("-pluginUUID <UUID>", "The UUID that the Elgato StreamDeck software knows this plugin as.", CommandOptionType.SingleValue);
+                var optionRegisterEvent = app.Option("-registerEvent <REGEVENT>", "The registration event", CommandOptionType.SingleValue);
+                var optionInfo = app.Option("-info <INFO>", "Some information", CommandOptionType.SingleValue);
 
-		try
-		{
-		  return Initialize(optionPort.ParsedValue, optionPluginUUID.Values[0], optionRegisterEvent.Values[0], optionInfo.Values[0], loggerFactory);
-		}
-		catch
-		{
-		  throw new ArgumentException($"{nameof(commandLineArgs)} must be the commandline args that the StreamDeck application calls this program with.");
-		}
-	  }
-	}
+                app.Parse(commandLineArgs);
 
-	public static ConnectionManager Initialize(int port, string uuid, string registerEvent, string info, ILoggerFactory loggerFactory)
-	{
-	  // TODO: Validate the info parameter
-	  var myInfo = JsonConvert.DeserializeObject<Messages.Info>(info);
+                try
+                {
+                    return Initialize(optionPort.ParsedValue, optionPluginUUID.Values[0], optionRegisterEvent.Values[0], optionInfo.Values[0], loggerFactory);
+                }
+                catch
+                {
+                    throw new ArgumentException($"{nameof(commandLineArgs)} must be the commandline args that the StreamDeck application calls this program with.");
+                }
+            }
+        }
 
-	  _LoggerFactory = loggerFactory;
-	  _Logger = loggerFactory?.CreateLogger("ConnectionManager") ?? NullLogger.Instance;
+        public static ConnectionManager Initialize(int port, string uuid, string registerEvent, string info, ILoggerFactory loggerFactory)
+        {
+            // TODO: Validate the info parameter
+            var myInfo = JsonConvert.DeserializeObject<Messages.Info>(info);
 
-	  var manager = new ConnectionManager()
-	  {
-		_Port = port,
-		_Uuid = uuid,
-		_RegisterEvent = registerEvent,
-		Info = myInfo
-	  };
+            _LoggerFactory = loggerFactory;
+            _Logger = loggerFactory?.CreateLogger("ConnectionManager") ?? NullLogger.Instance;
 
-	  return manager;
-	}
+            var manager = new ConnectionManager()
+            {
+                _Port = port,
+                _Uuid = uuid,
+                _RegisterEvent = registerEvent,
+                Info = myInfo
+            };
 
-	public ConnectionManager SetPlugin(BaseStreamDeckPlugin plugin)
-	{
-	  this._Plugin = plugin;
-	  plugin.Manager = this;
-	  return this;
-	}
+            return manager;
+        }
 
-	public async Task<ConnectionManager> StartAsync(CancellationToken token)
-	{
-	  TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+        public ConnectionManager SetPlugin(BaseStreamDeckPlugin plugin)
+        {
+            this._Plugin = plugin;
+            plugin.Manager = this;
+            return this;
+        }
 
-	  await Task.Factory.StartNew(() => Run(token), TaskCreationOptions.LongRunning);
+        public async Task<ConnectionManager> StartAsync(CancellationToken token)
+        {
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
 
-	  return this;
-	}
+            await Task.Factory.StartNew(() => Run(token), TaskCreationOptions.LongRunning);
 
-	private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
-	{
-	  throw new NotImplementedException();
-	}
+            return this;
+        }
 
-	private async Task Run(CancellationToken token)
-	{
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task Run(CancellationToken token)
+        {
 
 
 
-	  await _Socket.ConnectAsync(new Uri($"ws://localhost:{_Port}"), token);
-	  await _Socket.SendAsync(GetPluginRegistrationBytes(), WebSocketMessageType.Text, true, CancellationToken.None);
+            await _Socket.ConnectAsync(new Uri($"ws://localhost:{_Port}"), token);
+            await _Socket.SendAsync(GetPluginRegistrationBytes(), WebSocketMessageType.Text, true, CancellationToken.None);
 
-	  var keepRunning = true;
+            var keepRunning = true;
 
-	  while (!token.IsCancellationRequested)
-	  {
-		// Exit loop if the socket is closed or aborted
-		switch (_Socket.State)
-		{
-		  case WebSocketState.CloseReceived:
-		  case WebSocketState.Closed:
-		  case WebSocketState.Aborted:
-			keepRunning = false;
-			break;
-		}
-		if (!keepRunning) break;
+            while (!token.IsCancellationRequested)
+            {
+                // Exit loop if the socket is closed or aborted
+                switch (_Socket.State)
+                {
+                    case WebSocketState.CloseReceived:
+                    case WebSocketState.Closed:
+                    case WebSocketState.Aborted:
+                        keepRunning = false;
+                        break;
+                }
+                if (!keepRunning) break;
 
-		var jsonString = await GetMessageAsString(token);
+                var jsonString = await GetMessageAsString(token);
 
-		if (!string.IsNullOrEmpty(jsonString) && !jsonString.StartsWith("\0"))
-		{
-		  try
-		  {
-			var msg = JsonConvert.DeserializeObject<StreamDeckEventPayload>(jsonString);
-			if (msg == null)
-			{
-			  _Logger.LogError($"Unknown message received: {jsonString}");
-			  continue;
-			}
-			if (!_ActionDictionary.ContainsKey(msg.Event))
-			{
-			  _Logger.LogWarning($"Plugin does not handle the event '{msg.Event}'");
-			  continue;
-			}
-			_ActionDictionary[msg.Event]?.Invoke(_Plugin, msg);
-		  }
-		  catch (Exception ex)
-		  {
-			_Logger.LogError(ex, "Error while processing payload from StreamDeck");
-		  }
-		}
+                if (!string.IsNullOrEmpty(jsonString) && !jsonString.StartsWith("\0"))
+                {
+                    try
+                    {
+                        var msg = JsonConvert.DeserializeObject<StreamDeckEventPayload>(jsonString);
+                        if (msg == null)
+                        {
+                            _Logger.LogError($"Unknown message received: {jsonString}");
+                            continue;
+                        }
+                        if (!_ActionDictionary.ContainsKey(msg.Event))
+                        {
+                            _Logger.LogWarning($"Plugin does not handle the event '{msg.Event}'");
+                            continue;
+                        }
+                        _ActionDictionary[msg.Event]?.Invoke(_Plugin, msg);
+                    }
+                    catch (Exception ex)
+                    {
+                        _Logger.LogError(ex, "Error while processing payload from StreamDeck");
+                    }
+                }
 
-		await Task.Delay(100);
-	  }
-	  Dispose();
-	}
+                await Task.Delay(100);
+            }
+            Dispose();
+        }
 
-	#region StreamDeck Methods
+        #region StreamDeck Methods
 
-	public async Task SetTitleAsync(string context, string newTitle)
-	{
-	  var args = new SetTitleArgs()
-	  {
-		context = context,
-		payload = new SetTitleArgs.Payload
-		{
-		  title = newTitle,
-		  TargetType = SetTitleArgs.TargetType.HardwareAndSoftware
-		}
-	  };
+        public async Task SetTitleAsync(string context, string newTitle)
+        {
+            var args = new SetTitleArgs()
+            {
+                context = context,
+                payload = new SetTitleArgs.Payload
+                {
+                    title = newTitle,
+                    TargetType = SetTitleArgs.TargetType.HardwareAndSoftware
+                }
+            };
 
-	  await SendStreamDeckEvent(args);
-	}
+            await SendStreamDeckEvent(args);
+        }
 
-	public async Task SetSettingsAsync(string context, dynamic value)
-	{
-	  var args = new SetSettingsArgs()
-	  {
-		context = context,
-		payload = value
-	  };
-	  await SendStreamDeckEvent(args);
-	}
+        public async Task SetSettingsAsync(string context, dynamic value)
+        {
+            var args = new SetSettingsArgs()
+            {
+                context = context,
+                payload = value
+            };
+            await SendStreamDeckEvent(args);
+        }
 
-	public async Task ShowAlertAsync(string context)
-	{
-	  var args = new ShowAlertArgs()
-	  {
-		context = context
-	  };
-	  await SendStreamDeckEvent(args);
-	}
+        public async Task ShowAlertAsync(string context)
+        {
+            var args = new ShowAlertArgs()
+            {
+                context = context
+            };
+            await SendStreamDeckEvent(args);
+        }
 
-	public async Task ShowOkAsync(string context)
-	{
-	  var args = new ShowOkArgs()
-	  {
-		context = context
-	  };
-	  await SendStreamDeckEvent(args);
-	}
+        public async Task ShowOkAsync(string context)
+        {
+            var args = new ShowOkArgs()
+            {
+                context = context
+            };
+            await SendStreamDeckEvent(args);
+        }
 
-	public async Task OpenUrlAsync(string context, string url)
-	{
-	  var args = new OpenUrlArgs()
-	  {
-		context = context,
-		payload = new OpenUrlArgs.Payload()
-		{
-		  url = url
-		}
-	  };
-	  await SendStreamDeckEvent(args);
-	}
+        public async Task OpenUrlAsync(string context, string url)
+        {
+            var args = new OpenUrlArgs()
+            {
+                context = context,
+                payload = new OpenUrlArgs.Payload()
+                {
+                    url = url
+                }
+            };
+            await SendStreamDeckEvent(args);
+        }
 
-	#endregion
+        #endregion
 
-	private Task SendStreamDeckEvent(BaseStreamDeckArgs args)
-	{
-	  var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(args));
-	  return _Socket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
-	}
+        private Task SendStreamDeckEvent(BaseStreamDeckArgs args)
+        {
+            var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(args));
+            return _Socket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
 
-	private async Task<string> GetMessageAsString(CancellationToken token)
-	{
-	  var buffer = new byte[65536];
-	  var segment = new ArraySegment<byte>(buffer, 0, buffer.Length);
-	  await _Socket.ReceiveAsync(segment, token);
-	  var jsonString = Encoding.UTF8.GetString(buffer);
-	  return jsonString;
-	}
+        private async Task<string> GetMessageAsString(CancellationToken token)
+        {
+            var buffer = new byte[65536];
+            var segment = new ArraySegment<byte>(buffer, 0, buffer.Length);
+            await _Socket.ReceiveAsync(segment, token);
+            var jsonString = Encoding.UTF8.GetString(buffer);
+            return jsonString;
+        }
 
-	private ArraySegment<byte> GetPluginRegistrationBytes()
-	{
-	  var registration = new Messages.Info.PluginRegistration
-	  {
-		@event = _RegisterEvent,
-		uuid = _Uuid
-	  };
+        private ArraySegment<byte> GetPluginRegistrationBytes()
+        {
+            var registration = new Messages.Info.PluginRegistration
+            {
+                @event = _RegisterEvent,
+                uuid = _Uuid
+            };
 
-	  var outString = JsonConvert.SerializeObject(registration);
-	  var outBytes = Encoding.UTF8.GetBytes(outString);
+            var outString = JsonConvert.SerializeObject(registration);
+            var outBytes = Encoding.UTF8.GetBytes(outString);
 
-	  return new ArraySegment<byte>(outBytes);
-	}
+            return new ArraySegment<byte>(outBytes);
+        }
 
-	#region IDisposable Support
+        #region IDisposable Support
 
-	private bool disposedValue = false; // To detect redundant calls
-	private BaseStreamDeckPlugin _Plugin;
-	private static ILoggerFactory _LoggerFactory;
-	private static ILogger _Logger;
+        private bool disposedValue = false; // To detect redundant calls
+        private BaseStreamDeckPlugin _Plugin;
+        private static ILoggerFactory _LoggerFactory;
+        private static ILogger _Logger;
 
-	void Dispose(bool disposing)
-	{
-	  if (!disposedValue)
-	  {
-		if (disposing)
-		{
-		  _Socket.Dispose();
-		  _LoggerFactory.Dispose();
-		}
+        void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _Socket.Dispose();
+                    _LoggerFactory.Dispose();
+                }
 
-		disposedValue = true;
-	  }
-	}
+                disposedValue = true;
+            }
+        }
 
-	// This code added to correctly implement the disposable pattern.
-	public void Dispose()
-	{
-	  // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-	  Dispose(true);
-	  // TODO: uncomment the following line if the finalizer is overridden above.
-	  // GC.SuppressFinalize(this);
-	}
-	#endregion
-  }
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
 }
