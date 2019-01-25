d69aba526d245434e0352deef9c25dcf1068aece
Brady Gaster <bradyg@microsoft.com>
2019-01-25 09:06:13 -0800

    more updates

--- src/SamplePlugin/RegisterPluginAndStartStreamDeck.sh
+++ src/SamplePlugin/RegisterPluginAndStartStreamDeck.sh
@@ -6,7 +6,7 @@ pluginName=$(echo "$uuid" | tr -d \" | sed -e "s/$suffix$//")
 echo 'Installing the $pluginName plugin...'
 rm -r ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
 mkdir ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
-cp bin/Debug/netcoreapp2.2/* ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
+cp bin/Debug/netcoreapp2.2/osx-x64/* ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
 
 #start the stream deck
 open /Applications/Stream\ Deck.app &
