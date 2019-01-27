007ebdaa79b867e9622e8b7bb85d54177b937493
Wolfgang Stelzhammer <969932+Stelzi79@users.noreply.github.com>
2019-01-27 23:04:49 +0100

    changes copying the plugin files to creating a NTFS Junction
    Removes (commenting out) the copying of the bin files and instead creates a NTFS Junction to the bin folder.
    
    Needs testing - I still don't own a stream deck!

--- src/SamplePlugin/RegisterPluginAndStartStreamDeck.ps1
+++ src/SamplePlugin/RegisterPluginAndStartStreamDeck.ps1
@@ -1,4 +1,3 @@
-﻿
 $streamDeckExePath = "C:\Program Files\Elgato\StreamDeck\StreamDeck.exe"
 $bindir = $PSScriptRoot + "\bin\Debug\netcoreapp2.2"
 $manifestFile = $bindir +"\manifest.json"
@@ -13,8 +12,28 @@ $pluginName = split-path $PSScriptRoot -leaf
 
 Get-Process StreamDeck,$pluginName | Stop-Process –force -ErrorAction SilentlyContinue 
 
-mkdir $destDir -ErrorAction SilentlyContinue
-Get-ChildItem -Path $bindir -Recurse | ? { $_.FullName -inotmatch '.vs' } | Copy-Item -Destination $destDir
+# mkdir $destDir -ErrorAction SilentlyContinue
+# Copy content over
+# Get-ChildItem -Path $bindir -Recurse | ? { $_.FullName -inotmatch '.vs' } | Copy-Item -Destination $destDir
+
+if (!(Test-Path $destDir)) # Checks if Path exists
+{
+    # $destDir doesn't exist
+    cmd /c mklink /j $destDir $bindir # create the junction pointing to $binddir
+    # using the cmd /c mklink because a "New-Item" needs admin rights!
+    # this will also not work on any other system than Windows
+}
+else
+{
+    # something exists as $destDir
+    $junction = Get-Item $destDir
+    if (!($junction.Target.Contains($bindir))) # Checks if the $destDir is a junction and is pointing to the correct $bindir
+    {
+        # $destDir is either already a Junction but not targeting $bindir or it is someting else we can't use
+        Remove-Item -Path $destDir -Force -Recurse # remove whatever $destDir is
+        cmd /c mklink /j $destDir $bindir # creates the junction to $binddir
+    }    
+}
 
 Write-Host "Script Completed"
 
