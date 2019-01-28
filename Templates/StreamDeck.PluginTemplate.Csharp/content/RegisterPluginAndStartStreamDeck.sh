981907694ad482751b40f70eeb15c6393fc42cd1
derekforeman <derekforeman@gmail.com>
2019-01-28 15:02:57 -0500

    Register Plugin script (#54)
    - fixed the directory expansion with ${HOME}
    - Removed the escape character in the PLUGIN_PATH

--- Templates/StreamDeck.PluginTemplate.Csharp/content/RegisterPluginAndStartStreamDeck.sh
+++ Templates/StreamDeck.PluginTemplate.Csharp/content/RegisterPluginAndStartStreamDeck.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#Define Plugin Directory
-PLUGIN_DIR="~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/${pluginName.sdPlugin}"
 #Define suffix
 SUFFIX=".action"
 
@@ -16,19 +14,25 @@ uuid=$(sed -n 's/.*"UUID": "\(.*\)"/\1/p' manifest.json)
 
 pluginName=$(echo "${uuid}" | tr -d \" | sed -e "s/${SUFFIX}$//")
 
+#Define Plugin Directory
+PLUGIN_DIR="${HOME}/Library/Application Support/com.elgato.StreamDeck/Plugins/${pluginName}.sdPlugin"
+
 #Notify user of Action.
 echo "Installing the ${pluginName} plugin..."
 
 #On first run the file does not exist, we check if it exists.
-if [ -d ${PLUGIN_DIR} ]; then rm -fr ${PLUGIN_DIR}; fi
+[[ -d "${PLUGIN_DIR}" ]] && rm -fr "${PLUGIN_DIR}"
 
 #Recreate directory for plugin.
-mkdir -p ${PLUGIN_DIR}
+mkdir -p "${PLUGIN_DIR}"
 
 #Move our build into folder.
-cp bin/Debug/netcoreapp2.2/* ${PLUGIN_DIR}
+cp bin/Debug/netcoreapp2.2/osx-x64/* "${PLUGIN_DIR}"
 
 #Notify user of Action.
 echo "Done installing ${pluginName}."
+
+#start the stream deck
+open /Applications/Stream\ Deck.app & 
 
 exit
--- src/SamplePlugin/RegisterPluginAndStartStreamDeck.sh
+++ src/SamplePlugin/RegisterPluginAndStartStreamDeck.sh
@@ -1,14 +1,39 @@
+#!/usr/bin/env bash
+
+#Define suffix
+SUFFIX=".action"
+
+#Notify user of action.
 echo 'Killing the stream deck process'
+
+#Kill the process.
 pkill 'Stream Deck'
-uuid=$(jq .Actions[0].UUID manifest.json)
-suffix=".action"
-pluginName=$(echo "$uuid" | tr -d \" | sed -e "s/$suffix$//")
-echo 'Installing the $pluginName plugin...'
-rm -r ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
-mkdir ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
-cp bin/Debug/netcoreapp2.2/osx-x64/* ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/$pluginName.sdPlugin
+
+#Pull the UUID from the manifest.json
+uuid=$(sed -n 's/.*"UUID": "\(.*\)"/\1/p' manifest.json)
+
+pluginName=$(echo "${uuid}" | tr -d \" | sed -e "s/${SUFFIX}$//")
+
+#Define Plugin Directory
+PLUGIN_DIR="${HOME}/Library/Application Support/com.elgato.StreamDeck/Plugins/${pluginName}.sdPlugin"
+
+#Notify user of Action.
+echo "Installing the ${pluginName} plugin..."
+
+#On first run the file does not exist, we check if it exists.
+[[ -d "${PLUGIN_DIR}" ]] && rm -fr "${PLUGIN_DIR}"
+
+#Recreate directory for plugin.
+mkdir -p "${PLUGIN_DIR}"
+
+#Move our build into folder.
+cp bin/Debug/netcoreapp2.2/osx-x64/* "${PLUGIN_DIR}"
+
+#Notify user of Action.
+echo "Done installing ${pluginName}."
 
 #start the stream deck
-open /Applications/Stream\ Deck.app &
+open /Applications/Stream\ Deck.app & 
+
 
 exit
