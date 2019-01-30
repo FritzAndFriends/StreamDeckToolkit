7429467361cb557dad5a2ad5c44c631b45ee0e09
Hugo Dahl <hugo@code-jedi.com>
2019-01-30 07:48:14 -0600

    Add a debugger command line flag (#39)
    * Add a "-debug" command line parameter to have the plugin wait for a debugger
    
    * Display message when we are waiting on a debuger
    
    * Update SamplePlugin.csproj
    
    Remove the merged in `RuntimeIdentifiers` element. This is addressed separately in #56
    
    * Add-debugger-command-line-flag: Fix indentation and curly brace placement per Brady's review

--- src/StreamDeckLib/ConnectionManager.cs
+++ src/StreamDeckLib/ConnectionManager.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json;
 using StreamDeckLib.Messages;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.IO;
 using System.Net.WebSockets;
@@ -13,154 +14,213 @@ using System.Threading.Tasks;
 
 namespace StreamDeckLib
 {
-	/// <summary>
-	/// This class manages the connection to the StreamDeck hardware
-	/// </summary>
-	public partial class ConnectionManager : IDisposable
-	{
-		private int _Port;
-		private string _Uuid;
-		private string _RegisterEvent;
+    /// <summary>
+    /// This class manages the connection to the StreamDeck hardware
+    /// </summary>
+    public partial class ConnectionManager : IDisposable
+    {
+        private int _Port;
+        private string _Uuid;
+        private string _RegisterEvent;
 		private IStreamDeckProxy _Proxy;
-		private ConnectionManager() { }
-		public Messages.Info Info { get; private set; }
-		public static ConnectionManager Initialize(string[] commandLineArgs,
+        private ConnectionManager() { }
+        public Messages.Info Info { get; private set; }
+        public static ConnectionManager Initialize(string[] commandLineArgs,
 				ILoggerFactory loggerFactory = null, IStreamDeckProxy streamDeckProxy = null)
-		{
+        {
+			var waitForDebugger = false;
+            using (var app = new CommandLineApplication())
+            {
+                app.HelpOption();
 
-			using (var app = new CommandLineApplication())
-			{
-				app.HelpOption();
+                var optionPort = app.Option<int>("-port|--port <PORT>", "The port the Elgato StreamDeck software is listening on", CommandOptionType.SingleValue);
+                var optionPluginUUID = app.Option("-pluginUUID <UUID>", "The UUID that the Elgato StreamDeck software knows this plugin as.", CommandOptionType.SingleValue);
+                var optionRegisterEvent = app.Option("-registerEvent <REGEVENT>", "The registration event", CommandOptionType.SingleValue);
+                var optionInfo = app.Option("-info <INFO>", "Some information", CommandOptionType.SingleValue);
 
-				var optionPort = app.Option<int>("-port|--port <PORT>", "The port the Elgato StreamDeck software is listening on", CommandOptionType.SingleValue);
-				var optionPluginUUID = app.Option("-pluginUUID <UUID>", "The UUID that the Elgato StreamDeck software knows this plugin as.", CommandOptionType.SingleValue);
-				var optionRegisterEvent = app.Option("-registerEvent <REGEVENT>", "The registration event", CommandOptionType.SingleValue);
-				var optionInfo = app.Option("-info <INFO>", "Some information", CommandOptionType.SingleValue);
+#if DEBUG
+				var optionDebug = app.Option<bool>("-d|--debug", "Wait for a debugger to attach before execution", CommandOptionType.NoValue);
+#endif
 
-				app.Parse(commandLineArgs);
+                app.Parse(commandLineArgs);
 
-				try
+				// Validate that the command line parameters are valid, and display the help if they aren't.
+				if (app.GetValidationResult() != ValidationResult.Success)
 				{
-					return Initialize(optionPort.ParsedValue, optionPluginUUID.Values[0], optionRegisterEvent.Values[0], optionInfo.Values[0], loggerFactory, streamDeckProxy ?? new StreamDeckProxy());
+					app.ShowHelp();
+					throw new ArgumentException("The command line parameters were not valid or could not be parsed.");
 				}
-				catch
-				{
-					throw new ArgumentException($"{nameof(commandLineArgs)} must be the commandline args that the StreamDeck application calls this program with.");
-				}
-			}
-		}
+				
+#if DEBUG
+				waitForDebugger = optionDebug.HasValue() && optionDebug.ParsedValue;
+#endif
+				
+                try
+                {
+                    return Initialize(optionPort.ParsedValue, optionPluginUUID.Values[0], optionRegisterEvent.Values[0], optionInfo.Values[0], loggerFactory, streamDeckProxy ?? new StreamDeckProxy(), waitForDebugger);
+                }
+                catch
+                {
+					app.ShowHelp();
+					throw new ArgumentException($"{nameof(commandLineArgs)} must be the commandline args that the StreamDeck application calls this program with.", nameof(commandLineArgs));
+                }
+            }
+        }
 
-		private static ConnectionManager Initialize(int port, string uuid, string registerEvent, string info, ILoggerFactory loggerFactory, IStreamDeckProxy streamDeckProxy)
-		{
-			// TODO: Validate the info parameter
-			var myInfo = JsonConvert.DeserializeObject<Messages.Info>(info);
+        private static ConnectionManager Initialize(int port, string uuid, string registerEvent, string info, ILoggerFactory loggerFactory, IStreamDeckProxy streamDeckProxy, bool waitForDebugger = false)
+        {
+            // TODO: Validate the info parameter
+            var myInfo = JsonConvert.DeserializeObject<Messages.Info>(info);
 
-			_LoggerFactory = loggerFactory;
-			_Logger = loggerFactory?.CreateLogger("ConnectionManager") ?? NullLogger.Instance;
+            _LoggerFactory = loggerFactory;
+            _Logger = loggerFactory?.CreateLogger("ConnectionManager") ?? NullLogger.Instance;
 
-			var manager = new ConnectionManager()
-			{
-				_Port = port,
-				_Uuid = uuid,
-				_RegisterEvent = registerEvent,
+            var manager = new ConnectionManager()
+            {
+                _Port = port,
+                _Uuid = uuid,
+                _RegisterEvent = registerEvent,
 				Info = myInfo,
-				_Proxy = streamDeckProxy
-			};
+				_Proxy = streamDeckProxy,
+				_WaitForDebugger = waitForDebugger,
+            };
 
-			return manager;
-		}
+            return manager;
+        }
 
-		public ConnectionManager SetPlugin(BaseStreamDeckPlugin plugin)
-		{
-			this._Plugin = plugin;
-			plugin.Manager = this;
-			return this;
-		}
+        public ConnectionManager SetPlugin(BaseStreamDeckPlugin plugin)
+        {
+            this._Plugin = plugin;
+            plugin.Manager = this;
+            return this;
+        }
 
-		public async Task<ConnectionManager> StartAsync(CancellationToken token)
-		{
-			TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+        public async Task<ConnectionManager> StartAsync(CancellationToken token)
+        {
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
 
-			await Task.Factory.StartNew(() => Run(token), TaskCreationOptions.LongRunning);
+            await Task.Factory.StartNew(() => Run(token), TaskCreationOptions.LongRunning);
 
-			return this;
-		}
+            return this;
+        }
 
-		private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
-		{
-			throw new NotImplementedException();
-		}
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
 
-		private async Task Run(CancellationToken token)
-		{
+        private async Task Run(CancellationToken token)
+        {
+			await AttachDebugger(token);
 
+			// If a cancellation has been requested, let's not try to
+			// connect before we exit.
+			if (token.IsCancellationRequested)
+			{
+				return;
+			}
+			
 			await _Proxy.ConnectAsync(new Uri($"ws://localhost:{_Port}"), token);
 			await _Proxy.Register(_RegisterEvent, _Uuid);
 			
 
-			var keepRunning = true;
+            var keepRunning = true;
 
-			while (!token.IsCancellationRequested)
-			{
-				// Exit loop if the socket is closed or aborted
+            while (!token.IsCancellationRequested)
+            {
+                // Exit loop if the socket is closed or aborted
 				switch (_Proxy.State)
-				{
-					case WebSocketState.CloseReceived:
-					case WebSocketState.Closed:
-					case WebSocketState.Aborted:
-						keepRunning = false;
-						break;
-				}
-				if (!keepRunning) break;
+                {
+                    case WebSocketState.CloseReceived:
+                    case WebSocketState.Closed:
+                    case WebSocketState.Aborted:
+                        keepRunning = false;
+                        break;
+                }
+                if (!keepRunning) break;
 
 				var jsonString = await _Proxy.GetMessageAsString(token);
 
-				if (!string.IsNullOrEmpty(jsonString) && !jsonString.StartsWith("\0"))
-				{
-					try
-					{
-						var msg = JsonConvert.DeserializeObject<StreamDeckEventPayload>(jsonString);
-						if (msg == null)
-						{
-							_Logger.LogError($"Unknown message received: {jsonString}");
-							continue;
-						}
-						if (!_ActionDictionary.ContainsKey(msg.Event))
-						{
-							_Logger.LogWarning($"Plugin does not handle the event '{msg.Event}'");
-							continue;
-						}
-						_ActionDictionary[msg.Event]?.Invoke(_Plugin, msg);
-					}
-					catch (Exception ex)
-					{
-						_Logger.LogError(ex, "Error while processing payload from StreamDeck");
-					}
-				}
+                if (!string.IsNullOrEmpty(jsonString) && !jsonString.StartsWith("\0"))
+                {
+                    try
+                    {
+                        var msg = JsonConvert.DeserializeObject<StreamDeckEventPayload>(jsonString);
+                        if (msg == null)
+                        {
+                            _Logger.LogError($"Unknown message received: {jsonString}");
+                            continue;
+                        }
+                        if (!_ActionDictionary.ContainsKey(msg.Event))
+                        {
+                            _Logger.LogWarning($"Plugin does not handle the event '{msg.Event}'");
+                            continue;
+                        }
+                        _ActionDictionary[msg.Event]?.Invoke(_Plugin, msg);
+                    }
+                    catch (Exception ex)
+                    {
+                        _Logger.LogError(ex, "Error while processing payload from StreamDeck");
+                    }
+                }
 
-				await Task.Delay(100);
-			}
-			Dispose();
-		}
+                await Task.Delay(100);
+            }
+            Dispose();
+        }
 
-		#region StreamDeck Methods
-
-		public async Task SetTitleAsync(string context, string newTitle)
+		
+		/// <summary>
+		/// Waits in a loop for a debugger to attach to the process. Will return if either
+		/// a debugger instance is attached or a cancellation has been requested through
+		/// the <paramref name="token">cancellation token</paramref>.
+		/// </summary>
+		/// <param name="token">A cancellation token which can be used to break out of the wait loop.</param>
+		/// <returns>A value indicating whether a debugger was attached (<c>true</c>) or not (<c>false</c>).</returns>
+		private async Task<bool> AttachDebugger(CancellationToken token)
 		{
-			var args = new SetTitleArgs()
+			if (!_WaitForDebugger)
 			{
-				context = context,
-				payload = new SetTitleArgs.Payload
-				{
-					title = newTitle,
-					TargetType = SetTitleArgs.TargetType.HardwareAndSoftware
-				}
-			};
+				return true;
+			}
+
+			Console.Write("Attach a debugger to resume execution...");
+			
+			Debugger.Launch();
+
+			// Wait for a debugger to attach, but if the token gets a cancelation request, we need to exit.
+			while (!Debugger.IsAttached | !token.IsCancellationRequested)
+			{
+				// Polling every 1/2 second should be enough, rather than every 1/10th of a second.
+				// We could make this a parameter at a later time, but some validation would be needed
+				// to cap the minimum and maximum time allowed.
+				await Task.Delay(500, token); 
+			}
+
+			// If we exited because the token cancellation has been requested, return before doing any socket-related operations
+			return !token.IsCancellationRequested;
+		}
+		
+		
+        #region StreamDeck Methods
+
+        public async Task SetTitleAsync(string context, string newTitle)
+        {
+            var args = new SetTitleArgs()
+            {
+                context = context,
+                payload = new SetTitleArgs.Payload
+                {
+                    title = newTitle,
+                    TargetType = SetTitleArgs.TargetType.HardwareAndSoftware
+                }
+            };
 
 			await _Proxy.SendStreamDeckEvent(args);
 		}
 
-		public async Task SetImageAsync(string context, string imageLocation) {
+		public async Task SetImageAsync(string context, string imageLocation)
+		{
 
 			var imgString = Convert.ToBase64String(File.ReadAllBytes(imageLocation), Base64FormattingOptions.None);
 
@@ -194,17 +254,17 @@ namespace StreamDeckLib
 				context = context
 			};
 			await _Proxy.SendStreamDeckEvent(args);
-		}
+        }
 
-		public async Task SetSettingsAsync(string context, dynamic value)
-		{
-			var args = new SetSettingsArgs()
-			{
-				context = context,
-				payload = value
-			};
+        public async Task SetSettingsAsync(string context, dynamic value)
+        {
+            var args = new SetSettingsArgs()
+            {
+                context = context,
+                payload = value
+            };
 			await _Proxy.SendStreamDeckEvent(args);
-		}
+        }
 
 		public async Task SetStateAsync(string context, int state)
 		{
@@ -219,14 +279,13 @@ namespace StreamDeckLib
 			};
 
 			await _Proxy.SendStreamDeckEvent(args);
-
-
 		}
 
-		public async Task SendToPropertyInspectorAsync(string context, dynamic payload) {
+		public async Task SendToPropertyInspectorAsync(string context, dynamic payload)
+		{
 
 			var args = new SendToPropertyInspectorArgs
-			{
+        {
 				action = _Uuid,
 				context = context,
 				payload = payload
@@ -236,65 +295,67 @@ namespace StreamDeckLib
 
 		}
 
-		public async Task SwitchToProfileAsync(string context, string device, string profileName) {
+		public async Task SwitchToProfileAsync(string context, string device, string profileName)
+		{
 
 			var args = new SwitchToProfileArgs
-			{
+            {
 				context = context,
 				device = device,
 				payload = new SwitchToProfileArgs.Payload
 				{
 					profile = profileName
 				}
-			};
+            };
 
 			await _Proxy.SendStreamDeckEvent(args);
 
-		}
+        }
 
-		public async Task OpenUrlAsync(string context, string url)
-		{
-			var args = new OpenUrlArgs()
-			{
-				context = context,
-				payload = new OpenUrlArgs.Payload()
-				{
-					url = url
-				}
-			};
+        public async Task OpenUrlAsync(string context, string url)
+        {
+            var args = new OpenUrlArgs()
+            {
+                context = context,
+                payload = new OpenUrlArgs.Payload()
+                {
+                    url = url
+                }
+            };
 			await _Proxy.SendStreamDeckEvent(args);
-		}
+        }
 
-		#endregion
+        #endregion
 
-		#region IDisposable Support
+        #region IDisposable Support
 
-		private bool disposedValue = false; // To detect redundant calls
-		private BaseStreamDeckPlugin _Plugin;
-		private static ILoggerFactory _LoggerFactory;
-		private static ILogger _Logger;
+        private bool disposedValue = false; // To detect redundant calls
+        private BaseStreamDeckPlugin _Plugin;
+        private static ILoggerFactory _LoggerFactory;
+        private static ILogger _Logger;
+		private bool _WaitForDebugger;
 
-		void Dispose(bool disposing)
-		{
-			if (!disposedValue)
-			{
-				if (disposing)
-				{
+        void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
 					_Proxy.Dispose();
-				}
+                }
 
-				disposedValue = true;
-			}
-		}
+                disposedValue = true;
+            }
+        }
 
-		// This code added to correctly implement the disposable pattern.
-		public void Dispose()
-		{
-			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-			Dispose(true);
-			// TODO: uncomment the following line if the finalizer is overridden above.
-			// GC.SuppressFinalize(this);
-		}
-		#endregion
-	}
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
 }
