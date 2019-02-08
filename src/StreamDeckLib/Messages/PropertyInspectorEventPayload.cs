decf1ab987fed0d5e28502d957a8ae925a8f6d27
codingbandit <carey.payette@gmail.com>
2019-02-08 11:02:21 -0500

    Feature/sample property inspector (#95)
    * work in progress - sample property inspector
    
    * worked through intricacies of the deserialized dynamic payload
    
    * Updated Templates

---
+++ src/StreamDeckLib/Messages/PropertyInspectorEventPayload.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace StreamDeckLib.Messages
+{
+  public class PropertyInspectorEventPayload 
+  {
+	public string action { get; set; }
+	public string context { get; set; }
+	[JsonProperty(PropertyName = "event")]
+	public string Event { get; set; }
+	public dynamic payload { get; set; }
+  }
+}
