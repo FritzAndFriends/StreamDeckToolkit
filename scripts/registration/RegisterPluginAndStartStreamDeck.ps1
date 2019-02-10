aec78418710e4411faac123d194958c9242fab69
Carey Payette <carey.payette@gmail.com>
2019-02-10 16:35:00 -0500

    introduced the most recent powershell changes into this version of the script

--- scripts/registration/RegisterPluginAndStartStreamDeck.ps1
+++ scripts/registration/RegisterPluginAndStartStreamDeck.ps1
@@ -1,16 +1,11 @@
 ﻿Write-Host "Gathering deployment items..."
 
-Write-Host "Script root: $PSScriptRoot`n"
+Write-Host "Current Working Directory: $(get-location)"
 
-$basePath = $PSScriptRoot
+$basePath = $(get-location)
 
-if ($PSSCriptRoot.Length -eq 0) {
-  $basePath = $PWD.Path;
-}
-
-
-# Load and parse the plugin project file
-$pluginProjectFile = "$basePath\_StreamDeckPlugin_.csproj"
+# Load and parse the plugin project file - Fully expects there to be only one csproj file in there
+$pluginProjectFile = Get-ChildItem -Filter *.csproj | Select-Object -First 1
 $projectContent = Get-Content $pluginProjectFile | Out-String;
 $projectXML = [xml]$projectContent;
 
@@ -20,42 +15,46 @@ $buildConfiguration = "Debug"
 $targetFrameworkName = $projectXML.Project.PropertyGroup.TargetFramework;
 
 # Set local path references
-$streamDeckExePath = "$($ENV:ProgramFiles)\Elgato\StreamDeck\StreamDeck.exe"
-
-# For now, this PS script will only be run on Windows.
-$bindir = "$basePath\bin\Debug\$targetFrameworkName\win-x64"
+if ($IsMacOS) {
+  $streamDeckExePath = "/Applications/Stream Deck.app"
+  $bindir = "$basePath/bin/Debug/$targetFrameworkName/osx-x64"
+} else {
+  $streamDeckExePath = "$($ENV:ProgramFiles)\Elgato\StreamDeck\StreamDeck.exe"
+  $bindir = "$basePath\bin\Debug\$targetFrameworkName\win-x64"
+}
 
 # Make sure we actually have a directory/build to deploy
 If (-not (Test-Path $bindir)) {
-  Write-Error "The output directory `"$bindir`" was not found.`n You must first build the `"_StreamDeckPlugin_`" project before calling this script.";
+  Write-Error "The output directory `"$bindir`" was not found.`n You must first build the project before calling this script.";
   exit 1;
 }
 
 # Load and parse the plugin's manifest file
-$manifestFile = $bindir +"\manifest.json"
-$manifestContent = Get-Content $manifestFile | Out-String
-$json = ConvertFrom-JSON $manifestcontent
+$manifestPath = Join-Path $bindir "manifest.json"
+$json = Get-Content -Path $manifestPath -Raw | ConvertFrom-Json
 
 $uuidAction = $json.Actions[0].UUID
 
 $pluginID = $uuidAction.substring(0, $uuidAction.Length - ".action".Length)
-$destDir = "$($env:APPDATA)\Elgato\StreamDeck\Plugins\$pluginID.sdPlugin"
+
+if($IsMacOS) {
+  $destDir = "$HOME/Library/Application Support/com.elgato.StreamDeck/Plugins/$pluginID.sdPlugin"
+} else {
+  $destDir = "$($env:APPDATA)\Elgato\StreamDeck\Plugins\$pluginID.sdPlugin"
+}
 
 $pluginName = Split-Path $basePath -leaf
 
 Get-Process -Name ("StreamDeck", $pluginName) -ErrorAction SilentlyContinue | Stop-Process –force -ErrorAction SilentlyContinue
 
 # Delete the target directory, make sure the deployment/copy is clean
-If (Test-Path $destDir) {
-  Remove-Item -Recurse -Force -Path $destDir 
-}
+Remove-Item -Recurse -Force -Path $destDir -ErrorAction SilentlyContinue
+$bindir =  Join-Path $bindir "*"
 
 # Then copy all deployment items to the plugin directory
 New-Item -Type Directory -Path $destDir -ErrorAction SilentlyContinue # | Out-Null
-$bindir = $bindir +"\*"
 Copy-Item -Path $bindir -Destination $destDir -Recurse
 
-
-Write-Host "Deployment complete. Restarting the Stream Deck desktop application..."
-Start-Process $streamDeckExePath
+Write-Host "Deployment complete. We will NOT restart the Stream Deck desktop application here, but will from the template..."
+# Start-Process $streamDeckExePath
 exit 0
