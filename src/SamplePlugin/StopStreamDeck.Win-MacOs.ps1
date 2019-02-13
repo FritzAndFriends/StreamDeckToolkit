5122a9660259807eb9283b0bdc0c75b9560ff590
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2019-02-13 08:00:21 +0100

    Fixed file locking issue on Ntfs Junction deploy

---
+++ src/SamplePlugin/StopStreamDeck.Win-MacOs.ps1
@@ -0,0 +1,19 @@
+﻿param (
+		[string]$pluginName = ""
+)
+
+$basePath = $PSScriptRoot
+
+if (!($PSSCriptRoot)) {
+  $basePath = $PWD.Path;
+}
+
+if(!($pluginName)) {
+	$pluginName =	Split-Path $basePath -leaf
+}
+
+
+
+Write-Host "Stopping the processes 'StreamDeck' and '$pluginName' ..."
+Get-Process -Name ("StreamDeck", $pluginName) -ErrorAction SilentlyContinue | Stop-Process –force -ErrorAction SilentlyContinue
+Write-Host "Processes stopped"
