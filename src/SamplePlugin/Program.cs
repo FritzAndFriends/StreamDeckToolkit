827ce2b208fdf2988ee97a3f5875c4cab6715e33
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-02-15 12:36:35 -0500

    Added Default Configuration Builder (#118)
    * Refactored to an Proxy interface to address #47
    
    * Added remaining EventsSent to ConfigurationManager
    
    * Attempting to eliminate memory leak
    
    * Adding script to test the Plugin template
    
    * WIP
    
    * Updated to be more cross-platform friendly
    
    * Re-added cheer graffiti
    
    * Cleaning up the inner-loop build-debug-process
    
    * Addressed logger and memory leak issues #73 #114
    
    * Added a default configuration builder #34
    
     Addressed logger and memory leak issues #73 #114  (#117)
    
    * Refactored to an Proxy interface to address #47
    
    * Added remaining EventsSent to ConfigurationManager
    
    * Attempting to eliminate memory leak
    
    * Adding script to test the Plugin template
    
    * WIP
    
    * Updated to be more cross-platform friendly
    
    * Re-added cheer graffiti
    
    * Cleaning up the inner-loop build-debug-process
    
    * Addressed logger and memory leak issues #73 #114

--- src/SamplePlugin/Program.cs
+++ src/SamplePlugin/Program.cs
@@ -1,13 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Serilog;
-using StreamDeckLib;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
+﻿using StreamDeckLib;
 using System.Threading.Tasks;
 
 namespace SamplePlugin
@@ -19,80 +10,19 @@ namespace SamplePlugin
 		// Cheer 200 careypayette February 14, 2019
 		// Cheer 100 roberttables February 14, 2019
 		// Cheer 100 careypayette February 15, 2019
-		// Cheer 100 devlead 15/2/2019 
-
-		static ILoggerFactory GetLoggerFactory()
-		{
-			var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-			Directory.SetCurrentDirectory(dir);
-
-			var configuration = new ConfigurationBuilder()
-			                    .AddJsonFile("appsettings.json")
-			                    .Build();
-
-			Log.Logger = new LoggerConfiguration()
-			             .ReadFrom.Configuration(configuration)
-			             .CreateLogger();
-
-			var loggerFactory = new LoggerFactory()
-				.AddSerilog(Log.Logger);
-
-			TopLogger = loggerFactory.CreateLogger("top");
-
-			TopLogger.LogInformation("Plugin started");
-
-			return loggerFactory;
-		}
-
-		private static Microsoft.Extensions.Logging.ILogger TopLogger;
+		// Cheer 100 devlead 15/2/2019
 
 		static async Task Main(string[] args)
 		{
 
-#if DEBUG
-			// This gives us our "first chance" debugging, before even parsing the command
-			// line args, without the need to manually edit the code to toggle the feature
-			// ability on or off.
-
-			if (args.Select(arg => arg.Replace("--", "-"))
-			        .Any(arg => arg.Equals("-break")))
+			using (var config = StreamDeckLib.Config.ConfigurationBuilder.BuildDefaultConfiguration(args))
 			{
-				Console.WriteLine("Debugging has been requested. Waiting for a debugger to attach...");
-				Debugger.Launch();
 
-				while (!Debugger.IsAttached)
-				{
-					await Task.Delay(500);
-					Console.Write(".");
-				}
-			}
-#endif
-
-				using (var loggerFactory = GetLoggerFactory())
-				{
-					try
-					{
-						// codingbandit: I had to take out the using statement as it was causing the dispose method to be called (and disposing the socket connection)
-						await ConnectionManager.Initialize(args, loggerFactory)
-                                   .RegisterAction(new MySampleAction())
-						                       .StartAsync();
-
-					}
-					catch (Exception ex)
-					{
-						TopLogger.LogError(ex, "Error while running the plugin");
-					}
+				await ConnectionManager.Initialize(args, config.LoggerFactory)
+															 .RegisterAction(new MySampleAction())
+															 .StartAsync();
 
 			}
-
-#if DEBUG
-			if (Debugger.IsAttached)
-			{
-				// If a debugger is attached, give the developer a last chance to inspect
-				// variables, state, etc. before the process terminates.
-				Debugger.Break();
-			}
-#endif
 
 		}
 	}
