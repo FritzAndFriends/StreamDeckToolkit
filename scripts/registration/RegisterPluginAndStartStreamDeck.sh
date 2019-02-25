f865f32729401201c1bd2b2ded4cdaa2ace883da
Hugo Dahl <hugo@code-jedi.com>
2019-02-25 10:14:06 -0600

    Update scripts and git rules to use correct platform EOL for .sh files

--- scripts/registration/RegisterPluginAndStartStreamDeck.sh
+++ scripts/registration/RegisterPluginAndStartStreamDeck.sh
@@ -1,38 +1,38 @@
+#!bin/sh
+
 #Notify user we are killing the process
 echo 'Killing the Stream Deck process'
 
 # Kill the process
 pkill 'Stream Deck'
 
-# Pull the UUID from the manifest.json
-uuid=$(sed -n 's/.*"UUID": "\(.*\)"/\1/p' manifest.json)
-
-# Pull the plugin name from the UUID
-pluginName=${uuid%.*}
+# When creating a project from the template, this will be set at code generation time.
+pluginUUID="com.csharpfritz.samplePlugin"
 
 # Set the plugins and project directories to a variable
 pluginsDir="$HOME/Library/Application Support/com.elgato.StreamDeck/Plugins"
 projectDir=$(PWD)
 
 #Notify user we are installing the plugin to the plugins directory
-echo "Installing the $pluginName plugin to $pluginsDir"
+echo "Installing the SamplePlugin plugin to $pluginsDir"
 
 # Push the plugins directory on the stack
 pushd "$pluginsDir"
 # Check if the plugin direcotyr exists and remove it
-[ -d "$pluginName.sdPlugin" ] && rm -r $pluginName.sdPlugin
+[ -d "$pluginUUID.sdPlugin" ] && rm -r $pluginUUID.sdPlugin
 # Create the plugins directory
-mkdir $pluginName.sdPlugin
+mkdir $pluginUUID.sdPlugin
 # Copy the debug build output to the plugins directory
-cp -R "$projectDir/bin/Debug/netcoreapp2.2/osx-x64/." $pluginName.sdPlugin
+cp -R "$projectDir/bin/Debug/netcoreapp2.2/osx-x64/." $pluginUUID.sdPlugin
 # Pop the plugins directory off the stack returning to where we were
 popd
 
 #Notify user we successfully installed the plugin
-echo "Done installing ${pluginName}"
+echo "Done installing SamplePlugin"
 
 # Reopen the Stream Deck app and background it
-open /Applications/Stream\ Deck.app &
+# WILL NOT restart the Stream Deck software from the sample plugin, just as in the .PS1 script.
+#open /Applications/Stream\ Deck.app &
 
 # terminiate execution of the script
 exit
