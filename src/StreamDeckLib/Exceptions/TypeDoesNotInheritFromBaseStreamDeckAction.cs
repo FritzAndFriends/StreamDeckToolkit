5628a7564ec362ab95ffbd7c0ac790218257a0e3
Hugo Dahl <hugo@code-jedi.com>
2019-02-24 22:17:19 -0600

    Implement registration and instantiation methods of ActionManager. Includes tests.

---
+++ src/StreamDeckLib/Exceptions/TypeDoesNotInheritFromBaseStreamDeckAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace StreamDeckLib
+{
+  [Serializable]
+  internal class TypeDoesNotInheritFromBaseStreamDeckAction : Exception
+  {
+    private string fullName;
+    private Assembly assembly;
+
+    public TypeDoesNotInheritFromBaseStreamDeckAction()
+    {
+    }
+
+    public TypeDoesNotInheritFromBaseStreamDeckAction(string message) : base(message)
+    {
+    }
+
+    public TypeDoesNotInheritFromBaseStreamDeckAction(string fullName, Assembly assembly) : this($"The type \"{fullName}\" from assembly \"{assembly.GetName()}\" does not inherit from required base class \"{nameof(BaseStreamDeckAction)\"."})
+    {
+      this.fullName = fullName;
+      this.assembly = assembly;
+    }
+
+    public TypeDoesNotInheritFromBaseStreamDeckAction(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected TypeDoesNotInheritFromBaseStreamDeckAction(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+  }
+}
