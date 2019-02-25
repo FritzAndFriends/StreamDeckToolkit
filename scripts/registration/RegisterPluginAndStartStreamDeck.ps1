4e4d26b2eaa500336bc327d2afe3a256307dd8c1
Hugo Dahl <hugo@code-jedi.com>
2019-02-25 09:56:54 -0600

    Make registration script from sample match format of template (.PS1)

--- scripts/registration/RegisterPluginAndStartStreamDeck.ps1
+++ scripts/registration/RegisterPluginAndStartStreamDeck.ps1
@@ -1,11 +1,16 @@
 ﻿Write-Host "Gathering deployment items..."
 
-Write-Host "Current Working Directory: $(get-location)"
+Write-Host "Script root: $PSScriptRoot`n"
 
-$basePath = $(get-location)
+$basePath = $PSScriptRoot
 
-# Load and parse the plugin project file - Fully expects there to be only one csproj file in there
-$pluginProjectFile = Get-ChildItem -Filter *.csproj | Select-Object -First 1
+if ($PSSCriptRoot.Length -eq 0) {
+  $basePath = $PWD.Path;
+}
+
+
+# Load and parse the plugin project file
+$pluginProjectFile = "$basePath\SamplePlugin.csproj"
 $projectContent = Get-Content $pluginProjectFile | Out-String;
 $projectXML = [xml]$projectContent;
 
@@ -25,34 +30,25 @@ if ($IsMacOS) {
 
 # Make sure we actually have a directory/build to deploy
 If (-not (Test-Path $bindir)) {
-  Write-Error "The output directory `"$bindir`" was not found.`n You must first build the project before calling this script.";
+  Write-Error "The output directory `"$bindir`" was not found.`n You must first build the `"SamplePlugin`" project before calling this script.";
   exit 1;
 }
 
-# Load and parse the plugin's manifest file
-$manifestPath = Join-Path $bindir "manifest.json"
-$json = Get-Content -Path $manifestPath -Raw | ConvertFrom-Json
-
-$uuidAction = $json.Actions[0].UUID
-
-$pluginID = $uuidAction.substring(0, $uuidAction.Length - ".action".Length)
-
-if($IsMacOS) {
-  $destDir = "$HOME/Library/Application Support/com.elgato.StreamDeck/Plugins/$pluginID.sdPlugin"
-} else {
-  $destDir = "$($env:APPDATA)\Elgato\StreamDeck\Plugins\$pluginID.sdPlugin"
-}
+  $pluginUUID = "com.csharpfritz.samplePlugin";
+  $destDir = "$($env:APPDATA)\Elgato\StreamDeck\Plugins\$pluginUUID.sdPlugin"
 
 $pluginName = Split-Path $basePath -leaf
 
 Get-Process -Name ("StreamDeck", $pluginName) -ErrorAction SilentlyContinue | Stop-Process –force -ErrorAction SilentlyContinue
 
 # Delete the target directory, make sure the deployment/copy is clean
-Remove-Item -Recurse -Force -Path $destDir -ErrorAction SilentlyContinue
-$bindir =  Join-Path $bindir "*"
+If (Test-Path $destDir) {
+  Remove-Item -Recurse -Force -Path $destDir
+}
 
 # Then copy all deployment items to the plugin directory
 New-Item -Type Directory -Path $destDir -ErrorAction SilentlyContinue # | Out-Null
+$bindir = $bindir +"\*"
 Copy-Item -Path $bindir -Destination $destDir -Recurse
 
 Write-Host "Deployment complete. We will NOT restart the Stream Deck desktop application here, but will from the template..."
