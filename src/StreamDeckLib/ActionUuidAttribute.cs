7f241a188add2d23787ade9e914cdd2cc9c6222b
Ivan Pointer <ipointer@certifid.com>
2021-08-06 14:28:39 -0500

    #1 - Add an [Obsolete] extension of the StreamDeckActionAttribute named ActionUuidAttribute so we don't immediately break people who update their NuGet packages.

---
+++ src/StreamDeckLib/ActionUuidAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StreamDeckLib
+{
+	/// <summary>
+	/// An extension of the <see cref="StreamDeckActionAttribute"/>, so that we don't immediately break people who update
+	/// their NuGet packages...
+	/// </summary>
+	[Obsolete("This is being replaced by StreamDeckActionAttribute, use that instead.")]
+	public class ActionUuidAttribute : StreamDeckActionAttribute
+	{
+
+	}
+}
